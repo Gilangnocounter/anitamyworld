5b0036d9f615a7207510c785a2cffde9f560217c
Langzc <115324908+Gilangnocounter@users.noreply.github.com>
2025-09-01 21:02:57 +0700

    Update heartText.js

--- js/heartText.js
+++ js/heartText.js
@@ -11,7 +11,7 @@ export class HeartText {
 
         // Configuration options
         this.config = {
-            text: 'Only For U, Ayu Anita Purnama ♡',
+            text: 'Only For U, Ayu Anita Purnama',
             size: 20,
             height: 2,
             curveSegments: 12,
